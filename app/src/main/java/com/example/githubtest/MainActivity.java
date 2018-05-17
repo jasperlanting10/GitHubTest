f44e12aba4ecb6220e744222d0c54dd3ef986a83
doupie93 <dennisdoup@gmail.com>
2018-05-17 09:16:02 +0200

    TestCommit2

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Mogguh");
+        System.out.println("Mogguhhhh Ties haalt koffie");
     }
 }
